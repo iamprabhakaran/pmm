4e0130624acd2b2c3c9dd9f0f93c4390ff886b6f
iamprabhakaran <35688582+iamprabhakaran@users.noreply.github.com>
2018-02-12 16:00:02 +0530

    Create prabha12\2\1816.c

---
+++ prabha12\2\1816.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<conio.h>
+ 
+int main(void) 
+ 
+{
+int i,low,high,j;
+printf("\n enter the start range:");
+scanf("%d",&low);
+printf("\n enter the end range");
+scanf("%d",&high);
+printf("\n the prime numbers between %d and %d are:",low,high);
+for(i=low;i<=high;i++)
+{
+for(j=2;j<=i;j++)
+{
+if(i%j==0)
+break;
+}
+if(i==j)
+printf("\n%d",j);
+}
+getch();
+return 0;
+}
