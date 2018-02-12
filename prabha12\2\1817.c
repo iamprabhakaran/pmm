9548bc6a458e932bf389d9fb9dfbc411f20e1173
iamprabhakaran <35688582+iamprabhakaran@users.noreply.github.com>
2018-02-12 16:03:52 +0530

    Create prabha12\2\1817.c

---
+++ prabha12\2\1817.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int arn,n=0,n1,rem,result=0;
+scanf("%d",&n1);
+arn=n1;
+while(arn!=0)
+{
+	arn=arn/10;
+	n++;
+}
+arn=n1;
+while(arn!=0)
+{
+	rem=arn%10;
+	result=result+pow(rem,n);
+	arn=arn/10;
+}
+if(result==n1)
+printf("\nArmstrong number");
+else
+printf("not an armstrong number");
+getch();
+
+return 0;
+}
